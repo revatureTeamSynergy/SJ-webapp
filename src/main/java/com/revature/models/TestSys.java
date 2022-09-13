d0e294f37adc72ca78b8c1da182ee064b225112e
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-09-12 20:23:11 -0400

    testing custom ORM implementation

--- src/main/java/com/revature/models/TestSys.java
+++ src/main/java/com/revature/models/TestSys.java
@@ -17,79 +17,11 @@ public class TestSys {
 //			.createEntityManagerFactory("postgres");
 
 	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
-		
-//		 Configuration configuration = new Configuration();
-//	        configuration.configure("hibernate.cfg.xml");
-//	        
-//	        
-//	        SessionFactory factory = configuration.buildSessionFactory();
-//	    
-//	        
-//	        Session session = HibernateUtil.getSession();
-//
-//	        Transaction tx = session.beginTransaction();
-		
-		
-//		Database db = new Database();
-//		
-//		db.setJdbcUrl("jdbc:postgresql://p1data.cdiwjc2x9nug.us-east-1.rds.amazonaws.com:5432/");
-//		db.setDatabaseName("postgres");
-//		db.setUser("postgres");
-//		db.setPassword("bubblegum");
-//		
-//		db.createTable(Money.class);
-//		
-//		Money money = new Money("moname");
-//		db.insert(money);
-//		Customer custy = new Customer();
-//		custy.setFirstName("bobie");
-//		custy.setLastName("huns");
-//		custy.setUserLoginId(1);
-		
-//		
-//		CustomerDao cDao = new CustomerDao();
-//		
-//		cDao.insert(custy);
-		
-//		StandardSqlMaker sqlMk = new StandardSqlMaker();
-//		StandardPojoInfo pjo = sqlMk.getPojoInfo(custy);
-//		System.out.println(pjo(Customer.class));
-		
-		
-//		Object colName = custy.getClass().getField("sqlId").get(Customer.class);
-
-//		System.out.println(value);
-		
-//			System.out.println("field " + " " + field.getName());
-//		}
-		
-		
-	  
+		  
 	      
 	     Customer s = Manager.get(2, Customer.class);
 		System.out.println(s.getFirstName().getValue());
-//		AccountDao aDao = new AccountDao();
-//		
-//		Account acc1 = new Account();
-//		acc1.setBalance(100);
-//		acc1.setAccountTypeId(1);
-//		
-//		aDao.addAccount(null)
-	
-//		UserLoginDao uDao = new UserLoginDao();
-//		
-//		UserLogin uLog = uDao.getUserLoginByUsername("bilboboy1");
-//		uLog.setUsername("bilboboy2");
-//		
-//		try {
-//		uDao.update(uLog);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		
-//		tx.commit();
-//	
-//		session.close();
+
 	}
 
 }
